7db0c61855017f9a0238348773af6dfff05e857e
Romain Coupey <schaft115@hotmail.com>
2020-05-09 14:08:02 +0200

    add article model

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
