e9b3e38714ffd3c4e4f5e260f5c7e2e8237ae215
Romain Coupey <schaft115@hotmail.com>
2020-05-09 14:50:58 +0200

    rails-wikinimous before front

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,17 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.all.reverse
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
@@ -18,14 +22,22 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    @article.update(article_params)
+    redirect_to article_path
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
