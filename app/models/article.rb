50804e837010e210e9a55db5ce75aee067822008
Romain Coupey <schaft115@hotmail.com>
2020-05-09 15:55:30 +0200

    rails-wikinimous before front

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ApplicationRecord
+
+  def teaser
+    if self.content.length > 50
+      "#{self.content.capitalize[0..50]}... >>"
+    else
+      self.content.capitalize
+    end
+  end
 end
